41762140ecd76ab63126e6166364109d7729b865
devid <sankalp.verma@iphtechnologies.com>
2023-02-09 10:52:49 +0530

    Initial commit

---
+++ NetflixClone/Controllers/General/AirplayDemoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AirplayDemoViewController.swift
+//  NetflixClone
+//
+//  Created by vivek shrivastwa on 20/05/22.
+//
+
+import UIKit
+
+class AirplayDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
